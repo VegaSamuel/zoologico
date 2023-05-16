013fda1c9386e40b598a7c8aa1367dee417a96e9
DESKTOP-DLT8M7N\Dfinn <samuelvega.122003@gmail.com>
2023-05-15 23:05:58 -0700

    Proyecto de NetBeans Creado

---
+++ Zoologico/src/main/java/org/itson/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.itson.main;
+
+/**
+ * Esta clase es la principal que se encarga solamente de iniciar el programa.
+ * @author Samuel Vega
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
